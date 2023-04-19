5707469ff318ddd43c4c669ebdd8169383f2b58a
Moskovojs <rihards.moskovojs05@gmail.com>
2023-04-19 12:21:27 +0300

    Sagatavots vis pirma skripta rakstisanai

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour {
+	public int kadruSk = 0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Uzsakta programma! Tu esi "+kadruSk);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kadrs "+kadruSk);
+		kadruSk++;
+	}
+}
