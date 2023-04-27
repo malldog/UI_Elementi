ae96620559bad118a438bf118d5e845715e2c30b
Moskovojs <rihards.moskovojs05@gmail.com>
2023-04-27 11:17:56 +0300

    pabeigts projekts

---
+++ Assets/Skripti/VirtulaGenerators.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtulaGenerators : MonoBehaviour {
+
+    public GameObject[] virtuluSagataves;
+    public float laikaIntervals = 1f;
+    public float minX, maxX;
+    private Transform krasnsTransform;
+
+
+    void Start() {
+        krasnsTransform = GetComponent<Transform>();
+    }
+    public void SagatavotVirtulus(bool stavoklis) {
+        if (stavoklis == true)
+            StartCoroutine(Generet());
+        else
+            StopAllCoroutines();
+    }
+
+    IEnumerator Generet() {
+        while (true)
+        {
+            minX= krasnsTransform.position.x - 15;
+            maxX= krasnsTransform.position.x + 15;
+            var cipars = Random.Range(minX, maxX);
+            var pozicija = new Vector2(cipars, transform.position.y);
+            GameObject virtulis = Instantiate(virtuluSagataves[Random.Range(0, virtuluSagataves.Length)], pozicija, Quaternion.identity, krasnsTransform);
+            yield return new WaitForSeconds(laikaIntervals);
+        }
+    
+    }
+}
--- README.md
+++ README.md
@@ -12,11 +12,11 @@ Projekts, kura ietvaros apgūsim UI elementu veidošanu un darbošanos ar tiem, 
 - [x] Toggle group komponentes lietojums
 - [x] Dropdown elementa lietojums
 - [x] Slider elementu lietojums
-- [ ] Audio source integrēšana
-- [ ] Scroll view elementu lietojums
-- [ ] Drag & drop funkcionalitātes ieviešana
-- [ ] Pārslēgšanās starp ainām
-- [ ] Objektu ģenerēšana
-- [ ] Projekta sagatavošana darbināšanai no darbvirsmas (.exe)
+- [x] Audio source integrēšana
+- [x] Scroll view elementu lietojums
+- [x] Drag & drop funkcionalitātes ieviešana
+- [x] Pārslēgšanās starp ainām
+- [x] Objektu ģenerēšana
+- [x] Projekta sagatavošana darbināšanai no darbvirsmas (.exe)
 ![MR Bean attels](https://th.bing.com/th/id/R.d1be602fd4764e5884065eaeae58c4d3?rik=rSZY6tZCrc1QEw&pid=ImgRaw&r=0)
 
